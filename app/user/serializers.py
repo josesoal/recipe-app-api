944e5597abc878bde15e22b32377e99a02208778
Jose Soncco <josesoal@gmail.com>
2022-03-09 19:18:32 -0300

    Added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -23,6 +23,17 @@ class UserSerializer(serializers.ModelSerializer):
     an instance model would be created with a password not set properly.
     '''
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
@@ -48,3 +59,9 @@ class AuthTokenSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
+
+    '''NOTE:
+    Default authentication is using username and password, if we dont
+    override validate function, it will rise an error requiring username.
+    Our validation is made using email and password.
+    '''
