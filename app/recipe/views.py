07a83502a838f1e416ab4a4567027633a287f814
Jose Soncco <josesoal@gmail.com>
2022-03-10 13:09:17 -0300

    Added create tag feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -17,7 +19,15 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
+    '''NOTE:
+    It is neccessary to override perform_create function in order
+    to set the authenticated user, otherwise would rise an integrity error.
+    '''
+
 
 '''NOTE:
-For the TagViewSet, it could be used ListAPIView generic class.
+For the TagViewSet, it could be used ListCreateAPIView generic class.
 '''
